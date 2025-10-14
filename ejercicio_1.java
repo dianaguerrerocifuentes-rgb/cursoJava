1738e2b9cdbaa3b76663527042dabdc77afe0d41
dianaguerrerocifuentes-rgb <dianaguerrerocifuentes@gmail.com>
2025-10-14 18:12:08 -0500

    Add files via upload

---
+++ ejercicio_1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Diana Guerrero 
+ */
+public class ejercicio_1 {
+
+  
+    public static void main(String[] args) {
+    //crear variables 
+    String username="Matilde";
+    int cofreUno=8;
+    int cofreDos=12;
+    int sumaDulces;
+    //proceso
+    sumaDulces=cofreUno + cofreDos;
+    
+    
+    
+    //salida
+    System.out.print(username +"ahora tiene en total de"+ sumaDulces +"caramelos magicos");
+    
+    
+        
+        
+        
+    }
+    
+}
---
+++ ejercicio_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+* @author Diana Guerrero 
+ */
+public class ejercicio_2 {
+
+    
+    public static void main(String[] args) {
+        //crear variables 
+         String username="Daniela";
+        double primereto=3.5;
+        double segundoreto= 4.2;
+         double tercereto=2.8;
+         double sumaretosmagicos;
+         
+        //proceso
+        sumaretosmagicos=(primereto+segundoreto+tercereto)/3;
+        
+        //salida
+        System.out.print(username +"ahora tu promedio final es"+ sumaretosmagicos +"Nivel superado");
+        
+        
+    }
+    
+}
