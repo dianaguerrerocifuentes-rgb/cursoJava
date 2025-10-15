34c743ef3549fccf6867928afc5ebaa47e0d799d
dianaguerrerocifuentes-rgb <dianaguerrerocifuentes@gmail.com>
2025-10-15 17:28:42 -0500

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciojava;
+
+/**
+ *
+ * @author Diana Crolina Guerrero
+ */
+public class ejercicio3 {
+
+    
+    public static void main(String[] args) {
+    //crear variables
+    double salariobase=2000000;
+    double saludmagica=2000000*0.04;
+    double protecciondepension=2000000*0.05;
+   double totalsalario;
+   
+    
+  //proceso
+     salariobase=salariobase-saludmagica;
+     totalsalario=salariobase- protecciondepension;   
+     
+     //salida 
+      System.out.print("ahora tu saldo final es de"+ totalsalario +"preparate para tu proxima aventura");
+        
+        
+        
+        
+    }
+    
+}
